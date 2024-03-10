955e9d14e371fef801f24c6519e1155a18c0f5cf
Simon Moore <simon.moore@cl.cam.ac.uk>
2024-03-10 14:06:57 +0000

    Log any uncought exceptions.

--- badger2040-swm/badger_os/main.py
+++ badger2040-swm/badger_os/main.py
@@ -1,2 +1,15 @@
-#import launcher  # noqa F401
-import cycle_computer
+import time
+import machine
+import cyclecomputer2
+
+try:
+    cyclecomputer2.cyclecomputer2()
+except Exception as e:
+    print(e)
+    with open("main.log", "w") as fw:
+        fw.write(str(e))
+#        fw.write(traceback.format_exc())
+
+print("Sleeping 10s before reboot")
+time.sleep(10)
+machine.reset()
