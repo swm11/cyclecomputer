44b01d504886b23270b323398122f2b7772a5759
Simon Moore <simon.moore@cl.cam.ac.uk>
2024-03-10 14:26:08 +0000

    Add copyright

--- badger2040-swm/badger_os/manifest.py
+++ badger2040-swm/badger_os/manifest.py
@@ -1,1 +1,6 @@
+#-----------------------------------------------------------------------------
+# Manifest of source files for Cycle Computer 2
+#-----------------------------------------------------------------------------
+# Copyright (c) Simon W. Moore, March 2024
+
 manifest = ["main.py", "manifest.py", "cyclecomputer2.py", "netbat.py", "movement.py", "cycledisplay.py"]
