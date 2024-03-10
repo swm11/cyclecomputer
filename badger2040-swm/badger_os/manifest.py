8cf00bdfb4ae276b532602c06293b87a292d655c
Simon Moore <simon.moore@cl.cam.ac.uk>
2024-03-10 14:22:19 +0000

    Remove download of old code.

--- badger2040-swm/badger_os/manifest.py
+++ badger2040-swm/badger_os/manifest.py
@@ -1,1 +1,1 @@
-manifest = ["main.py", "manifest.py", "cycle_computer.py", "cyclecomputer2.py", "netbat.py", "movement.py", "cycledisplay.py"]
+manifest = ["main.py", "manifest.py", "cyclecomputer2.py", "netbat.py", "movement.py", "cycledisplay.py"]
